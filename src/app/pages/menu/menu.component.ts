c966bcc801060b1588a7157b6057f28677ff856b
Saeed Shoaraye Nejati <sharomin@Saeeds-Air.hitronhub.home>
2024-01-21 20:23:35 -0500

    all layouts are done- ham fixed

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
-
+  constructor(private service:OrderDetailsService) { }
+  foodData:any;
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
 }
