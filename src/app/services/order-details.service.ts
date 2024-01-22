0fa921f5b90d12a1e2c35a07cfe5654404a553c4
Saeed Shoaraye Nejati <sharomin@Saeeds-Air.hitronhub.home>
2024-01-21 20:12:32 -0500

    all layouts are done

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -13,42 +13,42 @@ export class OrderDetailsService {
     {
       id:1,
       foodName:"Kitchen Island",
-      foodDetails:"Pan-fried masala paneer.",
+      foodDetails:"Call us for estimation",
       foodPrice:200,
       foodImg:"../../assets/img/1.jpeg"
     },
     {
       id:2,
       foodName:"Modern Cabinet",
-      foodDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
+      foodDetails:"Call us for estimation",
       foodPrice:369,
       foodImg:"../../assets/img/2.jpeg"
     },
     {
       id:3,
       foodName:"Kitchen Renovation",
-      foodDetails:"panner",
+      foodDetails:"Call us for estimation",
       foodPrice:149,
       foodImg:"../../assets/img/3.jpeg"
     },
     {
       id:4,
       foodName:"Counter Top",
-      foodDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
+      foodDetails:"Call us for estimation",
       foodPrice:140,
       foodImg:"../../assets/img/5.jpeg"
     },
     {
       id:5,
       foodName:"Indulgence Brownie",
-      foodDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
+      foodDetails:"Call us for estimation",
       foodPrice:105,
       foodImg:"../../assets/img/5.jpeg"
     },
     {
       id:6,
       foodName:"Oreo Cheesecake Ice Cream",
-      foodDetails:"Oreo ice cream",
+      foodDetails:"Call us for estimation",
       foodPrice:219,
       foodImg:"../../assets/img/5.jpeg"
     }
